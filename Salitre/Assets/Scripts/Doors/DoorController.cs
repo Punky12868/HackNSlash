4999aa1709cad677f1a698ac4fe7acd64b57ca2c
Punky12868 <diegob12868@gmail.com>
2023-11-16 01:54:00 -0300

    dor :3

--- Salitre/Assets/Scripts/Doors/DoorController.cs
+++ Salitre/Assets/Scripts/Doors/DoorController.cs
@@ -8,21 +8,37 @@ public class DoorController : MonoBehaviour
     [SerializeField] Material debugCanEnter;
     [SerializeField] Material debugCannotEnter;
 
+    Transform tpPoint;
     [SerializeField] Transform assignedDoor;
-    [SerializeField] Transform tpPoint;
     [SerializeField] bool canEnter;
 
     public UnityEvent OnEnter;
     private void Awake()
     {
-        if (assignedDoor == null || tpPoint == null || !canEnter)
+        if (assignedDoor == null || !canEnter)
+        {
+            canEnter = false;
+            GetComponent<Renderer>().material.color = debugCannotEnter.color;
+        }
+        else if (assignedDoor != null)
+        {
+            tpPoint = assignedDoor.GetChild(0);
+        }
+    }
+    private void Update()
+    {
+        if (!canEnter && GetComponent<Renderer>().material.color != debugCannotEnter.color)
         {
             GetComponent<Renderer>().material.color = debugCannotEnter.color;
+        }
+        else if (canEnter && GetComponent<Renderer>().material.color != debugCanEnter.color)
+        {
+            GetComponent<Renderer>().material.color = debugCanEnter.color;
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && canEnter)
         {
             FindObjectOfType<AllDoors>().currentDoor = this;
             OnEnter.Invoke();
